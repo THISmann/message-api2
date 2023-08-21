c47298c9d99f503c371a58aa468721cda450991e
THISmann <fuhetienne0@gmail.com>
2023-08-21 15:43:41 +0300

    vueX

--- front-end/src/views/RamdomPage.vue
+++ front-end/src/views/RamdomPage.vue
@@ -1,22 +1,76 @@
 <template>
-      <NavbarComponent />
-    <h1> ramdom page !!</h1>
+    <NavbarComponent />
+    <div class="min-h-screen  bg-gray-100 grid ">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-2">
+            <h1> ram !!! </h1>
+            <div class="md:w-4/4 sm:w-4/4 m-8 bg-white rounded-lg shadow-md p-3  ">
+                <h1> ramdom page !!</h1>
+            </div>
+        </div>
+    </div> 
+  <div class="bg-white">
+    <div class="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+      <div class="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+        <svg viewBox="0 0 1024 1024" class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0" aria-hidden="true">
+          <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.7" />
+          <defs>
+            <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+              <stop stop-color="#7775D6" />
+              <stop offset="1" stop-color="#E935C1" />
+            </radialGradient>
+          </defs>
+        </svg>
+        <div class="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+          <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl"> you can chose a random .<br />Start now.</h2>
+          <p class="mt-6 text-lg leading-8 text-gray-300"> get a ramdom restaurant.</p>
+          <div class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+            <a href="#" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Get started</a>
+          </div>
+        </div>
+        <div class="relative mt-16 h-80 lg:mt-8">
+            <h2> hello bro </h2> 
+            <CardComponent />
+        </div>
+      </div>
+    </div>
+  </div> 
+
 </template>
 <script>
 // import axios from 'axios';
 // import qs from 'qs';
 import NavbarComponent from '../components/NavbarComponent.vue';
+import CardComponent from '../components/CardComponent.vue';
 export default {
     components: {
-        NavbarComponent
+        NavbarComponent , CardComponent
     },
-    data(){
+    data() {
         return {
 
         };
     },
     methods: {
-        //
+        getRandomRestaurant(){
+
+
+        //      // Fetch or set the item data based on the itemId
+        // axios.get(`https://bandaumnikov.ru/api/test/site/get-view?id=${itemId}`, {
+        //     // params: this.axiosParams
+        // })
+        //     .then(response => {
+        //         this.details = response.data.data;
+        //         console.log(this.details.id)
+        //     })
+        //     .catch(error => {
+        //         console.error('Error fetching data:', error);
+        //     })
+        //     .finally(() => {
+        //         this.loading = false;
+        //         this.getMessageFromIdCafe(itemId);
+        //         this.getNoteFromIdCafe(itemId);
+        //     });
+        }
     },
 }
 
